6e8eb9353a45265d448172379c1fbe0c96881de7
Miras Karazhigitov <karazhigitov@kolesa.kz>
2021-09-11 10:01:59 +0600

    Initial Commit

---
+++ News-VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News-VIPER
+//
+//  Created by Miras Karazhigitov on 11.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
